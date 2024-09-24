fb63748c4041ca51ea841ecb96d1ab7dc7c9873e
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-09-24 22:00:57 +0200

    refactor: py file with model class

---
+++ model_architecture.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DigitRecognitionModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(1,6,3,1)
+    self.conv2 = nn.Conv2d(6,16,3,1)
+    self.fc1 = nn.Linear(5*5*16, 120)
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 10)
+
+  def forward(self, X):
+    # Convolution and pooling layers
+    X = F.relu(self.conv1(X))
+    X = F.max_pool2d(X,2,2)
+    X = F.relu(self.conv2(X))
+    X = F.max_pool2d(X,2,2)
+
+    # Re-view to flatten it out
+    X = X.view(-1, 16*5*5)
+
+    # Fully connected layers
+    X = F.relu(self.fc1(X))
+    X = F.relu(self.fc2(X))
+    X = self.fc3(X)
+
+    return F.log_softmax(X, dim=1)
