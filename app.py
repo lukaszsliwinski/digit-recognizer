6dfb53e36af35fbd621f9ee6572ac827651c6da9
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-10-29 15:11:58 +0100

    feat: pass image from client to server

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, request
-
-
+from flask import Flask, request, jsonify
 import torch
 from torchvision import transforms
 from PIL import Image
 import PIL.ImageOps
+import io
 
-import sys
-
-app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+app = Flask(__name__, static_folder='client/build', static_url_path='/')
 
 @app.errorhandler(404)
 def not_found(e):
@@ -18,23 +15,25 @@ def not_found(e):
 def index():
   return app.send_static_file('index.html')
 
-@app.route('/data')
-def data():
-  return {'test': ['test1', 'test2', 'test3']}
-
-@app.route('/api/predict', methods=['POST'])
-def predict():
+@app.route('/api/recognize', methods=['POST'])
+def recognize():
   if request.method == 'POST':
-    # img = request.files['img']
+    if 'img' not in request.files:
+      return jsonify({"error": "No image file in request"}), 400
+    
+    img = request.files['img']
 
-    # prepare model
+    # Prepare model
     model = torch.load("trained_model.pt", weights_only=False)
     model.eval()
 
-    # Load the grayscale image
-    image = Image.open("model/3.jpg").convert('L')  # Convert to grayscale
+    # Read the image into memory and convert to grayscale
+    img_bytes = img.read()
+    img = Image.open(io.BytesIO(img_bytes))
+    grayscale = img.convert('L')
 
-    inverted_image = PIL.ImageOps.invert(image)
+    # Invert colors
+    inverted_img = PIL.ImageOps.invert(grayscale)
 
     # Define the transform to resize the image and convert to tensor
     transform = transforms.Compose([
@@ -43,15 +42,14 @@ def predict():
     ])
 
     # Apply the transform
-    img_tensor = transform(inverted_image).view(1,1,28,28)
+    img_tensor = transform(inverted_img).view(1,1,28,28)
 
     # Predict digit
     with torch.no_grad():
       prediction = model(img_tensor.view(1,1,28,28))
 
-    predicted_number = prediction[0].argmax().item()
-    print(predicted_number, file=sys.stderr)
-    return {'predicted_number': predicted_number}
+    # Return recognized digit
+    return {'recognized_digit': prediction[0].argmax().item()}
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const getData = () => {
-    axios
-      .post('/api/predict')
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => console.error(error))
-  }
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [recognizedDigit, setRecognizedDigit] = useState<number | null>(null);
+
+  // Handle file selection
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  // Handle submit file
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      alert('No file');
+      return;
+    }
+
+    // Create form data to send the image file
+    const formData = new FormData();
+    formData.append('img', selectedFile);
+
+    // Send POST request
+    try {
+      const response = await axios.post('/api/recognize', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      setRecognizedDigit(response.data.recognized_digit);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
 
   return (
     <div>
-      <button onClick={getData}>predict</button>
+      <div>
+        <h1>Upload an Image</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="file" onChange={handleFileChange} accept="image/*" />
+          <button type="submit">recognize</button>
+        </form>
+      </div>
+      <h2>recognized digit: {recognizedDigit}</h2>
     </div>
   );
 }
