c1e43a76ee609015e3c357ee4a23ef571db8ddba
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-11-01 17:13:01 +0100

    feat: add image preview before send to endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,47 @@ import './App.css';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | undefined>(undefined);
   const [recognizedDigit, setRecognizedDigit] = useState<number | null>(null);
 
   // Handle file selection
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setSelectedFile(event.target.files[0]);
+    const file = event.target.files?.[0];
+    
+    if (file && ['image/jpeg', 'image/png'].includes(file.type)) {
+      setSelectedFile(file);
+
+      // Resize the image to 28x28px
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const img = new Image();
+
+        console.log(typeof reader.result);
+
+        img.src = reader.result as string;
+        
+        img.onload = () => {
+          // Use canvas to resize the image
+          const canvas = document.createElement('canvas');
+          canvas.width = 28;
+          canvas.height = 28;
+          const ctx = canvas.getContext('2d');
+          
+          
+          if (ctx) {
+            // Draw the image on the canvas
+            ctx.drawImage(img, 0, 0, 28, 28);
+            
+            // Convert the canvas to a data url and set as the preview
+            setPreviewUrl(canvas.toDataURL('image/png'));
+          }
+        }
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please select a valid JPG or PNG image.");
+      // TODO: keep button disabled and add snackbar when format is not valid
+
     }
   };
 
@@ -18,6 +53,8 @@ function App() {
     event.preventDefault();
 
     if (!selectedFile) {
+      // TODO: disable button when there is no image
+      
       alert('No file');
       return;
     }
@@ -40,12 +77,23 @@ function App() {
   return (
     <div>
       <div>
-        <h1>Upload an Image</h1>
+        <h1>Upload an Image (jpg/jpeg/png)</h1>
         <form onSubmit={handleSubmit}>
-          <input type="file" onChange={handleFileChange} accept="image/*" />
+          <input type="file" onChange={handleFileChange} accept=".jpg,.jpeg,.png" />
           <button type="submit">recognize</button>
         </form>
       </div>
+      <h3>how the app sees the image:</h3>
+      <img
+        src={previewUrl}
+        alt="Preview"
+        style={{
+          // TODO: move inline styles to css file
+          width: "200px",  // Display size
+          height: "200px", // Display size
+          imageRendering: "pixelated", // Keeps pixels sharp when upscaling
+        }}
+      />
       <h2>recognized digit: {recognizedDigit}</h2>
     </div>
   );
