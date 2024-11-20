c907246d55dd7c21d4c26cc2d717e21a18263450
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-11-20 15:46:19 +0100

    docs: update comments

--- app.py
+++ app.py
@@ -7,57 +7,53 @@ from PIL import Image, ImageOps
 
 app = Flask(__name__, static_folder='client/build', static_url_path='/')
 
-
 # Model initialization
 MODEL_PATH = 'trained_model.pt'
 model = torch.load(MODEL_PATH, weights_only=False)
 model.eval()
 
-
-# Function that converts an image to grayscale and tensor
+# Function to preprocess the image
 def transform_image(img_bytes):
-  # Convert image to grayscale
+  """
+  Convert an image to grayscale, invert colors, resize to 28x28, and transform into a tensor.
+  """
   img = Image.open(io.BytesIO(img_bytes))
   grayscale = img.convert('L')
-
-  # Invert colors
   inverted_img = ImageOps.invert(grayscale)
-
-  # Define the transform to resize the image and convert to tensor
   transform = transforms.Compose([
-    transforms.Resize((28, 28)),  # Resize to 28x28
-    transforms.ToTensor(),        # Convert to tensor and scale to [0, 1]
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
   ])
-
   return transform(inverted_img).view(1,1,28,28)
 
-
-# Render React app
+# Route to serve the React app
 @app.route('/')
 def index():
+  """
+  Serve the React app's main entry point.
+  """
   return app.send_static_file('index.html')
 
+# API endpoint for digit recognition
 @app.route('/api/recognize', methods=['POST'])
 def recognize():
+  """
+  Process the uploaded image, run it through the model, and return the predicted digit.
+  """
   if request.method == 'POST':
     if 'img' not in request.files:
       return jsonify({"error": "No image file in request"}), 400
-    
+
     try:
       img = request.files['img']
-
-      # Read the image into memory and transform to tensor
       img_bytes = img.read()
       img_tensor = transform_image(img_bytes)
-
-      # Predict digit
       with torch.no_grad():
         prediction = model(img_tensor.view(1,1,28,28))
-
-      # Return recognized digit
       return jsonify({'recognized_digit': prediction[0].argmax().item()}), 200
     except Exception as e:
       return jsonify({"error": f"Failed to process the image: {str(e)}"}), 500
 
+# Run Flask app
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
