39ab01dad1595024a685d98964b4368229239553
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-01-20 19:40:15 +0100

    fix: move alert to the top of page

--- client/src/components/Alert.tsx
+++ client/src/components/Alert.tsx
@@ -37,7 +37,7 @@ function Alert({ type, text }: IAlert) {
 
   return (
     <div
-      className={`fixed bottom-8 w-full transform ${bgClass} px-6 py-4 text-center text-xl text-white shadow-lg transition-all duration-300 ease-in-out ${visible ? 'scale-100 opacity-100' : 'scale-95 opacity-0'} `}
+      className={`fixed top-8 w-full transform ${bgClass} px-6 py-4 text-center text-xl text-white shadow-lg transition-all duration-300 ease-in-out ${visible ? 'scale-100 opacity-100' : 'scale-95 opacity-0'} `}
     >
       {text}
     </div>
