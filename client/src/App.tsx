c74979c3574d3b7006a785cc5ca5e1f11a35b66f
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-01-20 12:07:40 +0100

    feat: change the order of components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <AlertProvider>
       <main className="flex min-h-screen w-full items-center justify-center bg-background bg-cover bg-fixed bg-center">
         <div className="flex min-h-screen w-full flex-col items-center justify-evenly bg-white/10 p-2 shadow-xl backdrop-blur-lg xs:p-8 lg:h-[80vh] lg:min-h-[650px] lg:flex-row lg:p-0">
-          <Upload />
           <Draw />
+          <Upload />
         </div>
       </main>
     </AlertProvider>
